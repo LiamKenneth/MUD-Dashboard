35871303b823ab03383d3984d3b1bd1e81a1156a
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2017-08-16 12:49:00 +0100

    TSlint added, removed old ESlint

--- src/components/stats/stats.component.tsx
+++ src/components/stats/stats.component.tsx
@@ -1,27 +1,25 @@
 import * as React from 'react';
 
-type statProps = {
-    message: string,
-    stat: number | string,
-    prevStat: number
+interface IstatProps {
+    message: string;
+    stat: number | string;
+    prevStat: number;
 }
 
 class Stats extends React.Component<any, any> {
 
-    showStatDifference(stat: number, prevStat: number): string {
+    public showStatDifference(stat: number, prevStat: number): string {
 
         const diff = stat - prevStat;
 
         if (diff > 0) {
-            return "(+" + diff + ")";
-        } else if (diff < 0) {
-             return "(" + diff + ")";
-        }
-
-        return "(" + diff + ")";
+            return `(+${diff})`;
+        } 
+        
+        return `(${diff})`;
     }
 
-    statHtml(stat: number | string, prevStat: number): any {
+    public statHtml(stat: number | string, prevStat: number): any {
          if (prevStat) {
             return <span>{stat} {this.showStatDifference(stat as number, prevStat)} {prevStat}</span>;
          }
@@ -29,11 +27,11 @@ class Stats extends React.Component<any, any> {
          return <span>{stat}</span>;
     }
 
-    formatStat(stat: number | string, prevStat: number): string {
+    public formatStat(stat: number | string, prevStat: number): string {
         return this.statHtml(stat, prevStat);
     }
 
-    render(): any {
+    public render(): any {
         return (
          <div>{this.props.message}{this.formatStat(this.props.stat, this.props.prevStat)}</div>
         );
