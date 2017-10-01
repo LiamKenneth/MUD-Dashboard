2d94d603a9ba485327b9fca664e91ee79eaf9222
Liam Kenneth <liam.kenneth@hotmail.co.uk>
2017-10-01 22:10:07 +0100

    wtf react

--- src/components/Charts/classes-component.tsx
+++ src/components/Charts/classes-component.tsx
@@ -1,45 +1,48 @@
 import * as React from 'react';
-import {ResponsiveContainer, PieChart, Pie, Legend, Cell} from 'recharts';
+import { ResponsiveContainer, PieChart, Pie, Legend, Cell } from 'recharts';
 
 class Classes extends React.Component<any, any> {
   constructor(props: any) {
     super(props);
 
     this.state = {
-      classes:  [],
+      classes: [],
     };
 
   }
 
   componentDidMount() {
     fetch(`http://www.archaicquest.com/staging/api/GameStats/GetClassBreakdown`)
-        .then((response) => {
-            return response.json();
-        })
-        .then((json) => {
-            console.log(json);
-            this.setState({classes: [...json]});
-        })
-        .catch((exception) => {
-            console.log('Error fetching  data: ' + exception.message);
-        });
-}
-
+      .then((response) => {
+        return response.json();
+      })
+      .then((json) => {
+        console.log(json);
+        this.setState({ classes: json });
+      })
+      .catch((exception) => {
+        console.log('Error fetching  data: ' + exception.message);
+      });
+  }
 
   render() {
-        const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
-        
-        return  (
-        <ResponsiveContainer>
-        <PieChart>
-        <Legend verticalAlign="top" />
-          <Pie data={this.state.classes} fill="#8884d8"  label={true}>
-          {this.state.classes.map((entry: any, index: any) => <Cell key={index} fill={COLORS[index % COLORS.length]}/>)}
-          </Pie>
-        </PieChart>
-       </ResponsiveContainer>
-        );
-    }
+    const COLORS = ['#0088FE', '#00C49F', '#FFBB28', '#FF8042'];
+
+    return this.state.classes.length > 1 ?
+    (
+    <div>
+          <ResponsiveContainer height="100%" width="100%">
+            <PieChart>
+              <Legend verticalAlign="top" />
+              <Pie data={this.state.classes} fill="#8884d8" label={true} dataKey="Name">
+                {this.state.classes.map((entry: any, index: any) => <Cell key={index} fill={COLORS[index % COLORS.length]} />)}
+              </Pie>
+            </PieChart>
+          </ResponsiveContainer>
+    </div>) : (
+      <div>Loading...</div>
+    );
   }
+}
 
 export default Classes;
